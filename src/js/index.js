239282723a702016cc05b2514a272953741cf31c
mgtablante <mgtablantel@gmail.com>
2021-04-13 20:14:58 +0000

    sub prueba

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import react from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -8,8 +9,19 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
-
+function SimpleCounter(props) {
+	return (
+		<div className="container">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="five">{props.digitFive}</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="three">{props.digitTree}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="one">{props.digitOne}</div>
+		</div>
+	);
+}
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
